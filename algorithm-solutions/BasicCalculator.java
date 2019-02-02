5280bcf8a703aabf839c39ffce9af53ba6b80a3d
Aditya Randive <adityaden5@gmail.com>
2019-02-01 18:36:39 -0800

    Create BasicCalculator.java

---
+++ algorithm-solutions/BasicCalculator.java
@@ -0,0 +1,51 @@
+class BasicCalculator {
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<Integer>();
+        
+        int result = 0;
+        int number = 0;
+        int sign = 1;
+        
+        for(int i=0;i<s.length();i++)
+        {
+            char c = s.charAt(i);
+            
+            if(Character.isDigit(c))
+            {
+                number = 10 * number + (int)(c - '0');
+            }
+            else if(c == '+')
+            {
+                result = result + sign * number;
+                number = 0;
+                sign = 1;
+            }
+            else if(c == '-')
+            {
+                result = result + sign * number;
+                number = 0;
+                sign = -1;
+            }
+            else if(c == '(')
+            {
+                stack.push(result);
+                stack.push(sign);
+                sign = 1;
+                result = 0;
+            }
+            else if(c == ')')
+            {
+                result = result + sign * number;
+                number = 0;
+                result = result * stack.pop();
+                result = result + stack.pop();
+            }
+        }
+        if(number != 0)
+        {
+            result = result + sign * number;
+        }
+        
+        return result;
+    }
+}
